880f18d1aa0c5dd50afbfc26cb7d9a5e834dec9a
hannasahlberg <hannassahlberg@live.se>
2026-03-20 11:53:32 +0100

    små ändringar

--- src/api/current.tsx
+++ src/api/current.tsx
@@ -1,12 +1,16 @@
 import { CurrentWeatherData } from "../types/current";
 
+const apiKey = process.env.EXPO_PUBLIC_API_KEY!;
 
-export const getData = async (city: string): Promise<CurrentWeatherData> =>{
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=36fd3b52f4a6105009462efb76f61f1c&units=metric&q=${city}`);
-        if (!response.ok) {
-            throw new Error("Failed to fetch weather data");
-        }
-        const data: CurrentWeatherData = await response.json();
-        return data; 
+export const getData = async (city: string): Promise<CurrentWeatherData> => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`,
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch weather data");
+  }
+
+  const data: CurrentWeatherData = await response.json();
+  return data;
 };
-    
--- src/api/forecast.tsx
+++ src/api/forecast.tsx
@@ -1,7 +1,9 @@
 import { ForecastData } from "../types/forecast";
     
+const apiKey = process.env.EXPO_PUBLIC_API_KEY!;
+
 export const getData = async (city: string): Promise<ForecastData> =>{
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=36fd3b52f4a6105009462efb76f61f1c&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`);
         if (!response.ok) {
             throw new Error("Failed to fetch weather data");
         }
